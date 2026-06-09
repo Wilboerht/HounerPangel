fecdc3a0aee18bfdf7637fc3234aaeb5a07cef9b
Hank <hongkunw2002@gmail.com>
2026-06-10 01:50:12 +0800

    1

--- src/data/travel/index.ts
+++ src/data/travel/index.ts
@@ -1,32 +1,10 @@
-import { beijingPlan } from "./beijing";
 import { nanjingPlan } from "./nanjing";
 import { hongkongPlan } from "./hongkong";
-import { shenzhenPlan } from "./shenzhen";
-import { guangzhouPlan } from "./guangzhou";
-import { chengduPlan } from "./chengdu";
-import { chongqingPlan } from "./chongqing";
-import { macauPlan } from "./macau";
-import { yangzhouPlan } from "./yangzhou";
-import { datongPlan } from "./datong";
-import { changbaishanPlan } from "./changbaishan";
-import { yunnanPlan } from "./yunnan";
-import { northwestPlan } from "./northwest";
 import { japanFullPlan } from "./japan-full";
 
 export const cnPlans = [
   nanjingPlan,
   hongkongPlan,
-  shenzhenPlan,
-  yangzhouPlan,
-  guangzhouPlan,
-  chengduPlan,
-  chongqingPlan,
-  macauPlan,
-  datongPlan,
-  changbaishanPlan,
-  yunnanPlan,
-  northwestPlan,
-  beijingPlan,
 ];
 
 export const jpPlans = [
